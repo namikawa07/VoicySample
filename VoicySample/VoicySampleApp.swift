e7b803559099010eb051894c56851db1f528038c
namikawa07 <tatsuki111407@gmail.com>
2023-08-15 23:46:05 +0900

    Initial Commit

---
+++ VoicySample/VoicySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicySampleApp.swift
+//  VoicySample
+//
+//  Created by 並川樹 on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct VoicySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
